287f55591f07747c823ac1f1fbcbe4d3e1fe193e
Gladibeth <gladibethdelos@gmail>
2019-08-20 17:04:34 -0400

    Add home, country, post and recommended

---
+++ docs/assets/js/main.js
@@ -0,0 +1,102 @@
+$(function () {
+  'use strict'
+
+  $('[data-toggle="offcanvas"]').on('click', function () {
+    $('.offcanvas-collapse').toggleClass('open')
+  })
+})
+
+
+
+
+// Menú fixed
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 70 && ($(window).width() >= 768)) {
+    $('.navbar-fixed-js').addClass('fixed');
+    $('.nav-link').addClass('fixed-color');
+    $('.main-nav__search').addClass('main-nav__search--scroll');
+    $('.dropdown-menu').addClass('dropdown-menu--scroll');
+    $('.dropdown-item').addClass('dropdown-item--scroll');
+    $("#iso").addClass('img-size').attr('src', 'assets/img/logo-b.png').removeClass('scroll-up');
+
+  } else {
+    $('.navbar-fixed-js').removeClass('fixed');
+    $('.nav-link').removeClass('fixed-color');
+    $('.main-nav__search').removeClass('main-nav__search--scroll');
+    $('.dropdown-menu').removeClass('dropdown-menu--scroll');
+    $('.dropdown-item').removeClass('dropdown-item--scroll');
+    $("#iso").removeClass('img-size').attr('src', 'assets/img/logo.png').removeClass('scroll-up');
+
+  }
+});
+
+$('.main-slider__img').click(function () {
+  var img = $('.slick-active').next().find('.main-banner__img img').attr('src');
+  $('.main-slider__img--active').attr('src', img);
+})
+
+
+$('.main-banner__content').slick({
+  infinite: true,
+  autoplay: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: false,
+  responsive: [{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 1000
+      }
+    }
+  ]
+});
+
+$('.main-destiny__content').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  arrows: true,
+  slidesToShow: 1,
+  responsive: [{
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+$('.dropdown-toggle').dropdown()
