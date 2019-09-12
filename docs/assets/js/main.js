40526fb02ded999ff623ee29936b29574bc2a07e
Gladibeth <gladibethdelos@gmail>
2019-09-12 12:10:23 -0400

    Add lazy load

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -35,6 +35,9 @@ $(window).scroll(function () {
 $('.main-slider__img').click(function () {
   var img = $('.slick-active').next().find('.main-banner__img img').attr('src');
   $('.main-slider__img--active').attr('src', img);
+
+  var text = $('.slick-active').next().find('.main-banner__number span').html();
+  $('.main-banner__number')
 })
 
 
@@ -99,29 +102,4 @@ $('.main-destiny__content').slick({
       }
     }
   ]
-});
-
-$(document).ready(function () {
-  // Add smooth scrolling to all links
-  $("a").on('click', function (event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function () {
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
 });
