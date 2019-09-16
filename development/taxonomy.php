952d00d83078b4d35173369fcb7a82b5c410e54f
renny10037 <renny10037@gmail.com>
2019-09-16 18:04:54 -0500

    colocando dinamico las taxonomias incluyendo link

--- development/taxonomy.php
+++ development/taxonomy.php
@@ -1,21 +1,25 @@
 <?php get_header(); //Obtener el header
-//   $subcategory = array(); //Array que permite obtener las subcategorias de recomendaciones y paises
-//   $taxonomy = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy')); //obtiene los datos de la taxonomia actual -->
-//   // Listado de categorias (Recomendaciones y países)
-//   $categories = get_categories(array(
-//       'parent'         => $taxonomy->term_id,
-//       'taxonomy' => 'tipo_destinos',
-//       'hide_empty' => false, //oculta categorias que no otenga post
-//       'order' => 'ASC',
-//       'post_status' => 'publish',
-//   )); 
-// ?>
-
+  $subcategory = array(); //Array que permite obtener las subcategorias de recomendaciones y paises
+  $taxonomy = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy')); //obtiene los datos de la taxonomia actual -->
+  // Listado de categorias (Recomendaciones y países)
+  $categories = get_categories(array(
+      'parent'         => $taxonomy->term_id,
+      'taxonomy' => 'tipo_destinos',
+      'hide_empty' => false, //oculta categorias que no otenga post
+      'order' => 'ASC',
+      'post_status' => 'publish',
+  )); 
+?>
+<?php foreach ($categories as $category) : ?>
+    <?php array_push($subcategory,$category->term_id); //obtiene el id de recomendaciones y países?>
+    <!--<h1><?php //echo ($category->name); ?></h1>-->
+<?php endforeach; 
+?>
 
 <section class="main-parallax" style="background-image: url('<?php echo get_field('image-category', $taxonomy); ?>');"> <!-- muestra la imagen dinamica de la taxonomia -->
     <div class="overlay"></div>
     <div class="main-parallax__title">
-      <h1>hola</h1>
+    <h1><?php echo $taxonomy->name; ?></h1>
     </div>
   </section>
   <section class="main-breadcrumb">
@@ -35,76 +39,32 @@
           <section class="main-articles" itemscope itemtype="http://schema.org/ScholarlyArticle">
             <div class="container">
               <div class="main-articles__content">
-                <div class="main-articles__item">
-                  <div class="main-articles__img">
-                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/Capadocia.jpg">
-                  </div>
-                  <div class="main-articles__title" itemprop="name">
-                    <p>Qué ver</p>
-                    <span>y que hacer?</span>
-                  </div>
-                  <div class="main-article__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
+                <?php
+                  // Listado de subcategorias países
+                  $subcategories = get_categories(array(
+                  'parent'         => $subcategory[1],
+                  'taxonomy' => 'tipo_destinos',
+                  'hide_empty' => false, //oculta categorias que no otenga post
+                  'order_by' => 'name',
+                  'order' => 'ASC',
+                  'post_status' => 'publish',
+                )); ?>
+                <?php foreach ($subcategories as $sub_category) : ?>
+                  <div class="main-articles__item">
+                    <div class="main-articles__img">
+                    <img class="img-round lazy" src="<?php echo get_field('image-category', $sub_category); ?>">
+                    </div>
+                    <div class="main-articles__title" itemprop="name">
+                      <p><?php echo ($sub_category->name); ?></p>
+                      <span><?php echo ($sub_category->description); ?></span>
+                    </div>
+                    <div class="main-article__btn">
+                      <div class="btn_custom btn--medium btn--filled">
+                      <a href="<?php echo bloginfo('url').'/'.$sub_category->taxonomy.'/'.$sub_category->slug;?>">Ver más</a>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div class="main-articles__item">
-                  <div class="main-articles__img">
-                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/Capadocia.jpg">
-                  </div>
-                  <div class="main-articles__title" itemprop="name">
-                    <p>Lugares</p>
-                    <span>recomendados</span>
-                  </div>
-                  <div class="main-article__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-articles__item">
-                  <div class="main-articles__img">
-                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/Capadocia.jpg">
-                  </div>
-                  <div class="main-articles__title" itemprop="name">
-                    <p>Parques</p>
-                    <span>u naturaleza</span>
-                  </div>
-                  <div class="main-article__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-articles__item">
-                  <div class="main-articles__img">
-                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/Capadocia.jpg">
-                  </div>
-                  <div class="main-articles__title" itemprop="name">
-                    <p>Islas</p>
-                    <span>y playas</span>
-                  </div>
-                  <div class="main-article__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-articles__item">
-                  <div class="main-articles__img">
-                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/Capadocia.jpg">
-                  </div>
-                  <div class="main-articles__title" itemprop="name">
-                    <p>Museos</p>
-                    <span>y monumentos</span>
-                  </div>
-                  <div class="main-article__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
+                <?php endforeach;?> 
               </div>
               <hr class="main-articles__line">
             </div>
@@ -114,112 +74,31 @@
           <section class="main-country main-country">
             <div class="container">
               <div class="main-country__content">
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/Capadocia.jpg">
-                  </div>
-                  <div class="main-country__title" itemprop="name">
-                    <p>Alemania</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
+                <?php
+                  // Listado de subcategorias recomendaciones
+                  $subcategories = get_categories(array(
+                  'parent'         => $subcategory[0],
+                  'taxonomy' => 'tipo_destinos',
+                  'hide_empty' => false, //oculta categorias que no otenga post
+                  'order_by' => 'name',
+                  'order' => 'ASC',
+                  'post_status' => 'publish',
+                )); ?>
+                <?php foreach ($subcategories as $sub_category) : ?>
+                  <div class="main-country__item">
+                    <div class="main-country__img">
+                    <img class="img-round lazy" src="<?php echo get_field('image-category', $sub_category); ?>">
+                    </div>
+                    <div class="main-country__title" itemprop="name">
+                    <p><?php echo ($sub_category->name); ?></p>
+                    </div>
+                    <div class="main-country__btn">
+                      <div class="btn_custom btn--medium btn--filled">
+                        <a href="<?php echo bloginfo('url').'/'.$sub_category->taxonomy.'/'.$sub_category->slug;?>">Ver más</a>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/Capadocia.jpg">
-                  </div>
-                  <div class="main-country__title" itemprop="name">
-                    <p>España</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/Capadocia.jpg">
-                  </div>
-                  <div class="main-country__title" itemprop="name">
-                    <p>Grecia</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/Capadocia.jpg">
-                  </div>
-                  <div class="main-country__title" itemprop="name">
-                    <p>Holanda</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/Capadocia.jpg">
-                  </div>
-                  <div class="main-country__title" itemprop="name">
-                    <p>Inglaterra</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/Capadocia.jpg">
-                  </div>
-                  <div class="main-country__title" itemprop="name">
-                    <p>Italia</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/Capadocia.jpg">
-                  </div>
-                  <div class="main-country__title" itemprop="name">
-                    <p>Republica</p>
-                    <span>checa</span>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/Capadocia.jpg">
-                  </div>
-                  <div class="main-country__title" itemprop="name">
-                    <p>Suiza</p>
-                    <span>checa</span>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
+                  <?php endforeach;?>
               </div>
               <hr class="main-articles__line">
             </div>
