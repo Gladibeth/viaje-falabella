79c625755064898bf2b5882781f0f6354ec0476c
renny10037 <renny10037@gmail.com>
2019-09-16 12:13:44 -0500

    colocando dinamico titulo, e imagen de las recomendaciones y países

--- development/taxonomy.php
+++ development/taxonomy.php
@@ -1,6 +1,23 @@
-<?php get_header(); ?>
-<?php $taxonomy = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy')); ?>
-<section class="main-parallax" style="background-image: url('<?php echo get_field('image-category', $taxonomy); ?>');">
+<?php get_header(); //Obtener el header
+  $subcategory = array(); //Array que permite obtener las subcategorias de recomendaciones y paises
+  $taxonomy = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy')); //obtiene los datos de la taxonomia actual -->
+  // Listado de categorias (Recomendaciones y países)
+  $categories = get_categories(array(
+      'parent'         => $taxonomy->term_id,
+      'taxonomy' => 'tipo_destinos',
+      'hide_empty' => false, //oculta categorias que no otenga post
+      'order' => 'ASC',
+      'post_status' => 'publish',
+  )); 
+?>
+
+<?php foreach ($categories as $category) : ?>
+    <?php array_push($subcategory,$category->term_id); //obtiene el id de recomendaciones y países?>
+    <!--<h1><?php //echo ($category->name); ?></h1>-->
+<?php endforeach; 
+?>
+
+<section class="main-parallax" style="background-image: url('<?php echo get_field('image-category', $taxonomy); ?>');"> <!-- muestra la imagen dinamica de la taxonomia -->
     <div class="overlay"></div>
     <div class="main-parallax__title">
       <h1><?php echo $taxonomy->name; ?></h1>
@@ -23,112 +40,31 @@
           <section class="main-country main-country">
             <div class="container">
               <div class="main-country__content">
+              <?php
+                // Listado de subcategorias países
+                $subcategories = get_categories(array(
+                'parent'         => $subcategory[1],
+                'taxonomy' => 'tipo_destinos',
+                'hide_empty' => false, //oculta categorias que no otenga post
+                'order_by' => 'name',
+                'order' => 'DESC',
+                'post_status' => 'publish',
+              )); ?>
+              <?php foreach ($subcategories as $sub_category) : ?>
                 <div class="main-country__item">
                   <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
+                    <img class="img-round lazy" src="<?php echo get_field('image-category', $sub_category); ?>">
                   </div>
                   <div class="main-country__title">
-                    <p>Alemania</p>
+                    <p><?php echo ($sub_category->name); ?></p>
                   </div>
                   <div class="main-country__btn">
                     <div class="btn_custom btn--medium btn--filled">
-                      Ver más
+                      <a href="<?php echo get_permalink();?>" target="_blank" rel="noopener noreferrer">Ver más </a>
                     </div>
                   </div>
                 </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>España</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Grecia</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Holanda</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Inglaterra</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Italia</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Republica</p>
-                    <span>checa</span>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Suiza</p>
-                    <span>checa</span>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
+                <?php endforeach;?> 
               </div>
               <hr class="main-articles__line">
             </div>
@@ -138,12 +74,23 @@
           <section class="main-country main-country">
             <div class="container">
               <div class="main-country__content">
+                <?php
+                // Listado de subcategorias recomendaciones
+                $subcategories = get_categories(array(
+                'parent'         => $subcategory[0],
+                'taxonomy' => 'tipo_destinos',
+                'hide_empty' => false, //oculta categorias que no otenga post
+                'order_by' => 'name',
+                'order' => 'ASC',
+                'post_status' => 'publish',
+              )); ?>
+              <?php foreach ($subcategories as $sub_category) : ?>
                 <div class="main-country__item">
                   <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
+                    <img class="img-round lazy" src="<?php echo get_field('image-category', $sub_category); ?>">
                   </div>
                   <div class="main-country__title">
-                    <p>Alemania</p>
+                    <p><?php echo ($sub_category->name); ?></p>
                   </div>
                   <div class="main-country__btn">
                     <div class="btn_custom btn--medium btn--filled">
@@ -151,99 +98,7 @@
                     </div>
                   </div>
                 </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>España</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Grecia</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Holanda</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Inglaterra</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Italia</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Republica</p>
-                    <span>checa</span>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Suiza</p>
-                    <span>checa</span>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
+                <?php endforeach;?> 
               </div>
               <hr class="main-articles__line">
             </div>
