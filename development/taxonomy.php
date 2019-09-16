992ec5695dc1c7c54d9ddf931c59a6a4388118c8
Gladibeth <gladibethdelos@gmail>
2019-09-16 14:41:31 -0400

    Add recommed

--- development/taxonomy.php
+++ development/taxonomy.php
@@ -19,119 +19,83 @@
       </ul>
       <div class="tab-content" id="pills-tabContent">
         <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-          <section class="main-country main-country">
-            <div class="container">
-              <div class="main-country__content">
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Alemania</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>España</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Grecia</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Holanda</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Inglaterra</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Italia</p>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Republica</p>
-                    <span>checa</span>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-                <div class="main-country__item">
-                  <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
-                  </div>
-                  <div class="main-country__title">
-                    <p>Suiza</p>
-                    <span>checa</span>
-                  </div>
-                  <div class="main-country__btn">
-                    <div class="btn_custom btn--medium btn--filled">
-                      Ver más
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <hr class="main-articles__line">
+          <section class="main-articles" itemscope itemtype="http://schema.org/ScholarlyArticle">
+    <div class="container">
+      <div class="main-articles__content">
+        <div class="main-articles__item">
+          <div class="main-articles__img">
+            <img class="img-round" src="assets/img/img-4.jpeg">
+          </div>
+          <div class="main-articles__title" itemprop="name">
+            <p>Qué ver</p>
+            <span>y que hacer?</span>
+          </div>
+          <div class="main-article__btn">
+            <div class="btn_custom btn--medium btn--filled">
+              Ver más
             </div>
-          </section>
+          </div>
+        </div>
+        <div class="main-articles__item">
+          <div class="main-articles__img">
+            <img class="img-round" src="assets/img/img-4.jpeg">
+          </div>
+          <div class="main-articles__title" itemprop="name">
+            <p>Lugares</p>
+            <span>recomendados</span>
+          </div>
+          <div class="main-article__btn">
+            <div class="btn_custom btn--medium btn--filled">
+              Ver más
+            </div>
+          </div>
+        </div>
+        <div class="main-articles__item">
+          <div class="main-articles__img">
+            <img class="img-round" src="assets/img/img-4.jpeg">
+          </div>
+          <div class="main-articles__title" itemprop="name">
+            <p>Parques</p>
+            <span>u naturaleza</span>
+          </div>
+          <div class="main-article__btn">
+            <div class="btn_custom btn--medium btn--filled">
+              Ver más
+            </div>
+          </div>
+        </div>
+        <div class="main-articles__item">
+          <div class="main-articles__img">
+            <img class="img-round" src="assets/img/img-4.jpeg">
+          </div>
+          <div class="main-articles__title" itemprop="name">
+            <p>Islas</p>
+            <span>y playas</span>
+          </div>
+          <div class="main-article__btn">
+            <div class="btn_custom btn--medium btn--filled">
+              Ver más
+            </div>
+          </div>
+        </div>
+        <div class="main-articles__item">
+          <div class="main-articles__img">
+            <img class="img-round" src="assets/img/img-4.jpeg">
+          </div>
+          <div class="main-articles__title" itemprop="name">
+            <p>Museos</p>
+            <span>y monumentos</span>
+          </div>
+          <div class="main-article__btn">
+            <div class="btn_custom btn--medium btn--filled">
+              Ver más
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr class="main-articles__line">
+    </div>
+  </section>
         </div>
         <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
           <section class="main-country main-country">
@@ -139,7 +103,7 @@
               <div class="main-country__content">
                 <div class="main-country__item">
                   <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
+                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
                   </div>
                   <div class="main-country__title">
                     <p>Alemania</p>
@@ -152,7 +116,7 @@
                 </div>
                 <div class="main-country__item">
                   <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
+                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
                   </div>
                   <div class="main-country__title">
                     <p>España</p>
@@ -165,7 +129,7 @@
                 </div>
                 <div class="main-country__item">
                   <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
+                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
                   </div>
                   <div class="main-country__title">
                     <p>Grecia</p>
@@ -178,7 +142,7 @@
                 </div>
                 <div class="main-country__item">
                   <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
+                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
                   </div>
                   <div class="main-country__title">
                     <p>Holanda</p>
@@ -191,7 +155,7 @@
                 </div>
                 <div class="main-country__item">
                   <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
+                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
                   </div>
                   <div class="main-country__title">
                     <p>Inglaterra</p>
@@ -204,7 +168,7 @@
                 </div>
                 <div class="main-country__item">
                   <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
+                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
                   </div>
                   <div class="main-country__title">
                     <p>Italia</p>
@@ -217,7 +181,7 @@
                 </div>
                 <div class="main-country__item">
                   <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
+                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
                   </div>
                   <div class="main-country__title">
                     <p>Republica</p>
@@ -231,7 +195,7 @@
                 </div>
                 <div class="main-country__item">
                   <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
+                    <img class="img-round" src="<?php echo get_template_directory_uri();?>/assets/img/img-4.jpeg">
                   </div>
                   <div class="main-country__title">
                     <p>Suiza</p>
