bc064ec17cbcd2c32e382305b0594e550bf6b00e
renny10037 <renny10037@gmail.com>
2019-09-17 17:07:35 -0500

    colocando taxonomy dinamica(titulo,descripcion e imagen,link)

--- development/taxonomy.php
+++ development/taxonomy.php
@@ -1,9 +1,11 @@
-<?php get_header(); ?>
+<?php get_header(); //Obtener el header
+  $taxonomy = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy')); //obtiene los datos de la taxonomia actual --> 
+?>
   <section class="main-parallax">
-    <div class="overlay"></div>
+    <div class="overlay" style="background-image: url('<?php echo get_field('image-category', $taxonomy); ?>');"></div> <!-- muestra la imagen dinamica de la taxonomia -->
     <div class="main-parallax__title main-parallax__title--post">
-      <h1>Qué ver</h1>
-      <span>y que hacer?</span>
+      <h1><?php echo $taxonomy->name;?></h1>
+      <span><?php echo $taxonomy->description;?></span>
       <div class="main-parallax__btn">
         <div class="btn_custom btn--medium btn--filled">
           Volver
@@ -31,53 +33,57 @@
         <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
           <div itemscope itemtype="http://schema.org/LiveBlogPosting" class="main-posts__content">
             <div class="main-posts__item" href="post.html">
-              <div class="main-posts__img">
-                <img itemprop="image" class="img-round" src="assets/img/img-4.jpeg">
-              </div>
-              <div class="main-posts__box">
-                <div class="main-posts__title">
-                  <p itemprop="name">
-                    Lorem ipsum
-                  </p>
+              <?php
+                $args = array(
+                  'post_type' => 'destinos'
+                  );
+                  $loop = new WP_Query( $args );
+              ?>
+              <?php while( $loop->have_posts() ) : $loop->the_post();?>
+                <div class="main-posts__img">
+                <img itemprop="image" class="img-round" src="<?php the_post_thumbnail_url(); ?>"/>
                 </div>
-                <div class="main-posts__autor">
-                  <div class="main-posts__name">
-                    <p itemprop="author">Gladibeth Franco</p>
+                <div class="main-posts__box">
+                  <div class="main-posts__title">
+                    <p itemprop="name"><?php the_title();?></p>
                   </div>
-                  <div class="main-posts__line"></div>
-                  <div class="main-posts__date">
-                    <span itemprop="datePublished">18/08/2019</span>
+                  <div class="main-posts__autor">
+                    <div class="main-posts__name">
+                      <p itemprop="author"><?php the_author();?></p>
+                    </div>
+                    <div class="main-posts__line"></div>
+                    <div class="main-posts__date">
+                      <span itemprop="datePublished"><?php the_date('d/m/y');?></span>
+                    </div>
+                  </div>
+                  <div class="main-posts__description">
+                    <p itemprop="description"><?php the_excerpt();?></p>
+                  </div>
+                  <div class="main-posts__btn">
+                    <div class="btn_custom btn--medium btn--filled">
+                    <a href="<?php the_permalink(); ?>">Ver más </a>
+                    </div>
+                  </div>
+                  <hr class="main-articles__line">
+                  <div class="main-posts__social">
+                    <div class="main-posts__comments">
+                      <p>999</p>
+                    </div>
+                    <div class="main-posts__tags">
+                      <p>tags</p>
+                      <p>tags</p>
+                    </div>
                   </div>
                 </div>
-                <div class="main-posts__description">
-                  <p itemprop="description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae obcaecati deserunt natus eius
-                    autem quia laborum dolor eaque debitis corporis iure, odit possimus suscipit delectus maxime
-                    voluptate similique rerum mollitia.
-                  </p>
-                </div>
-                <div class="main-posts__btn">
-                  <div class="btn_custom btn--medium btn--filled">
-                    Ver más
-                  </div>
-                </div>
-                <hr class="main-articles__line">
-                <div class="main-posts__social">
-                  <div class="main-posts__comments">
-                    <p>999</p>
-                  </div>
-                  <div class="main-posts__tags">
-                    <p>tags</p>
-                    <p>tags</p>
-                  </div>
-                </div>
-              </div>
+              <?php endwhile;
+                wp_reset_query();
+              ?>
             </div>
 
           </div>
         </div>
-        <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
-        <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+        <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">mas populares</div>
+        <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">mas comentarios</div>
       </div>
 
       <hr class="main-articles__line">
