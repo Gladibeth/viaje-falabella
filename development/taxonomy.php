53677f0d1bae8d0ce5bba92a33bb6d5ad34df345
renny10037 <renny10037@gmail.com>
2019-09-17 15:57:27 -0500

    agregando allpost en taxonomy y creando taxonomia europa

--- development/taxonomy.php
+++ development/taxonomy.php
@@ -1,112 +1,88 @@
-<?php get_header(); //Obtener el header
-  $subcategory = array(); //Array que permite obtener las subcategorias de recomendaciones y paises
-  $taxonomy = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy')); //obtiene los datos de la taxonomia actual -->
-  // Listado de categorias (Recomendaciones y países)
-  $categories = get_categories(array(
-      'parent'         => $taxonomy->term_id,
-      'taxonomy' => 'tipo_destinos',
-      'hide_empty' => false, //oculta categorias que no otenga post
-      'order' => 'ASC',
-      'post_status' => 'publish',
-  )); 
-?>
-<?php foreach ($categories as $category) : ?>
-    <?php array_push($subcategory,$category->term_id); //obtiene el id de recomendaciones y países?>
-    <!--<h1><?php //echo ($category->name); ?></h1>-->
-<?php endforeach; 
-?>
-
-<section class="main-parallax" style="background-image: url('<?php echo get_field('image-category', $taxonomy); ?>');"> <!-- muestra la imagen dinamica de la taxonomia -->
+<?php get_header(); ?>
+  <section class="main-parallax">
     <div class="overlay"></div>
-    <div class="main-parallax__title">
-    <h1><?php echo $taxonomy->name; ?></h1>
-    </div>
-  </section>
-  <section class="main-breadcrumb">
-    <div class="container">
-      <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-        <li class="nav-item">
-          <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Recomendaciones</a>
-        </li>
-        <span class="span_tabs">/</span>
-        <li class="nav-item">
-          <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Paises</a>
-        </li>
-       
-      </ul>
-      <div class="tab-content" id="pills-tabContent">
-        <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-          <section class="main-articles" itemscope itemtype="http://schema.org/ScholarlyArticle">
-            <div class="container">
-              <div class="main-articles__content">
-                <?php
-                  // Listado de subcategorias países
-                  $subcategories = get_categories(array(
-                  'parent'         => $subcategory[1],
-                  'taxonomy' => 'tipo_destinos',
-                  'hide_empty' => false, //oculta categorias que no otenga post
-                  'order_by' => 'name',
-                  'order' => 'ASC',
-                  'post_status' => 'publish',
-                )); ?>
-                <?php foreach ($subcategories as $sub_category) : ?>
-                  <div class="main-articles__item">
-                    <div class="main-articles__img">
-                    <img class="img-round lazy" src="<?php echo get_field('image-category', $sub_category); ?>">
-                    </div>
-                    <div class="main-articles__title" itemprop="name">
-                      <p><?php echo ($sub_category->name); ?></p>
-                      <span><?php echo ($sub_category->description); ?></span>
-                    </div>
-                    <div class="main-article__btn">
-                      <div class="btn_custom btn--medium btn--filled">
-                      <a href="<?php echo bloginfo('url').'/'.$sub_category->taxonomy.'/'.$sub_category->slug;?>">Ver más</a>
-                      </div>
-                    </div>
-                  </div>
-                <?php endforeach;?> 
-              </div>
-              <hr class="main-articles__line">
-            </div>
-          </section>
+    <div class="main-parallax__title main-parallax__title--post">
+      <h1>Qué ver</h1>
+      <span>y que hacer?</span>
+      <div class="main-parallax__btn">
+        <div class="btn_custom btn--medium btn--filled">
+          Volver
         </div>
-        <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
-          <section class="main-country main-country">
-            <div class="container">
-              <div class="main-country__content">
-                <?php
-                  // Listado de subcategorias recomendaciones
-                  $subcategories = get_categories(array(
-                  'parent'         => $subcategory[0],
-                  'taxonomy' => 'tipo_destinos',
-                  'hide_empty' => false, //oculta categorias que no otenga post
-                  'order_by' => 'name',
-                  'order' => 'ASC',
-                  'post_status' => 'publish',
-                )); ?>
-                <?php foreach ($subcategories as $sub_category) : ?>
-                  <div class="main-country__item">
-                    <div class="main-country__img">
-                    <img class="img-round lazy" src="<?php echo get_field('image-category', $sub_category); ?>">
-                    </div>
-                    <div class="main-country__title" itemprop="name">
-                    <p><?php echo ($sub_category->name); ?></p>
-                    </div>
-                    <div class="main-country__btn">
-                      <div class="btn_custom btn--medium btn--filled">
-                        <a href="<?php echo bloginfo('url').'/'.$sub_category->taxonomy.'/'.$sub_category->slug;?>">Ver más</a>
-                      </div>
-                    </div>
-                  </div>
-                  <?php endforeach;?>
-              </div>
-              <hr class="main-articles__line">
-            </div>
-          </section>
-        </div>
-      
       </div>
     </div>
   </section>
-  
+  <section class="main-posts">
+    <div class="container">
+      <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+        <li class="nav-item">
+          <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab"
+            aria-controls="pills-home" aria-selected="true">Más Recientes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab"
+            aria-controls="pills-profile" aria-selected="false">Más Populares</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab"
+            aria-controls="pills-contact" aria-selected="false">Más comentarios</a>
+        </li>
+      </ul>
+      <div class="tab-content" id="pills-tabContent">
+        <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+          <div itemscope itemtype="http://schema.org/LiveBlogPosting" class="main-posts__content">
+            <div class="main-posts__item" href="post.html">
+              <div class="main-posts__img">
+                <img itemprop="image" class="img-round" src="assets/img/img-4.jpeg">
+              </div>
+              <div class="main-posts__box">
+                <div class="main-posts__title">
+                  <p itemprop="name">
+                    Lorem ipsum
+                  </p>
+                </div>
+                <div class="main-posts__autor">
+                  <div class="main-posts__name">
+                    <p itemprop="author">Gladibeth Franco</p>
+                  </div>
+                  <div class="main-posts__line"></div>
+                  <div class="main-posts__date">
+                    <span itemprop="datePublished">18/08/2019</span>
+                  </div>
+                </div>
+                <div class="main-posts__description">
+                  <p itemprop="description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae obcaecati deserunt natus eius
+                    autem quia laborum dolor eaque debitis corporis iure, odit possimus suscipit delectus maxime
+                    voluptate similique rerum mollitia.
+                  </p>
+                </div>
+                <div class="main-posts__btn">
+                  <div class="btn_custom btn--medium btn--filled">
+                    Ver más
+                  </div>
+                </div>
+                <hr class="main-articles__line">
+                <div class="main-posts__social">
+                  <div class="main-posts__comments">
+                    <p>999</p>
+                  </div>
+                  <div class="main-posts__tags">
+                    <p>tags</p>
+                    <p>tags</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
+        <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+      </div>
+
+      <hr class="main-articles__line">
+    </div>
+  </section>
+
+
 <?php get_footer(); ?>
