776d81a0f53abed2f68cfcace7711f521204b73f
renny10037 <renny10037@gmail.com>
2019-10-01 15:31:14 -0500

    acomodando gitignore

---
+++ development/index.php
@@ -0,0 +1,415 @@
+<?php get_header(); ?>
+  
+  <div class="main-banner">
+    <div class="slider slider-for__banner">
+      <div class="main-banner__item">
+        <div class="main-banner__text">
+          <div class="main-banner__number">
+            <span>01</span>
+            <div class="main-banner-smaline"></div>
+          </div>
+          <div class="main-banner__title">
+            <p>Hola</p>
+            <span>ipsum</span>
+          </div>
+          <div class="main-banner__description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse ea molestias inventore? Consectetur doloribus aut minus error temporibus aliquam.</p>
+          </div>
+          <div class="main-banner__mail">
+            <div class="main-banner__boxmail">
+              <a href="">
+                <i class="fa fa-envelope"></i>
+                <p>mailing ipsum</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="main-banner__camara">
+          <div class="text__camara">
+            <p>Hello good morning</p>
+          </div>
+          <div class="icons__camara">
+            <i class="fa fa-camera" aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="main-banner__scroll">
+        <div class="scroll__container">
+          <a href="#destiny">
+           SCROLL
+          </a>
+        </div>
+        </div>
+        <div class="main-banner__img">
+          <img src="<?php echo get_template_directory_uri();?>/assets/img/recomendados-1.jpg">
+        </div>
+      </div>
+      <div class="main-banner__item">
+        <div class="main-banner__text">
+          <div class="main-banner__number">
+            <span>02</span>
+            <div class="main-banner-smaline"></div>
+          </div>
+          <div class="main-banner__title">
+            <p>como</p>
+            <span>ipsum</span>
+          </div>
+          <div class="main-banner__description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse ea molestias inventore? Consectetur doloribus aut minus error temporibus aliquam.</p>
+          </div>
+          <div class="main-banner__mail">
+            <div class="main-banner__boxmail">
+              <a href="">
+                <i class="fa fa-envelope"></i>
+                <p>mailing ipsum</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="main-banner__camara">
+          <div class="text__camara">
+            <p>Hello good morning</p>
+          </div>
+          <div class="icons__camara">
+            <i class="fa fa-camera" aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="main-banner__scroll">
+        <div class="scroll__container">
+          <a href="#destiny">
+           SCROLL
+          </a>
+        </div>
+        </div>
+        <div class="main-banner__img">
+          <img src="<?php echo get_template_directory_uri();?>/assets/img/articulo-3.jpg">
+        </div>
+      </div>
+      <div class="main-banner__item">
+        <div class="main-banner__text">
+          <div class="main-banner__number">
+            <span>03</span>
+            <div class="main-banner-smaline"></div>
+          </div>
+          <div class="main-banner__title">
+            <p>estas</p>
+            <span>ipsum</span>
+          </div>
+          <div class="main-banner__description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse ea molestias inventore? Consectetur doloribus aut minus error temporibus aliquam.</p>
+          </div>
+          <div class="main-banner__mail">
+            <div class="main-banner__boxmail">
+              <a href="">
+                <i class="fa fa-envelope"></i>
+                <p>mailing ipsum</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="main-banner__camara">
+          <div class="text__camara">
+            <p>Hello good morning</p>
+          </div>
+          <div class="icons__camara">
+            <i class="fa fa-camera" aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="main-banner__scroll">
+        <div class="scroll__container">
+          <a href="#destiny">
+           SCROLL
+          </a>
+        </div>
+        </div>
+        <div class="main-banner__img">
+          <img src="<?php echo get_template_directory_uri();?>/assets/img/Capadocia.jpg">
+        </div>
+      </div>
+      
+    </div>
+    <div class="slider slider-nav__banner">
+      <div>
+        <div class="main-slider__next">
+          <div class="main-slider__box">
+            <div class="main-slider__text">
+              <hr class="main-slider__line">
+              <div class="main-slider__number">
+                <span class="number--active">01</span>
+                <div class="main-slider-smaline"></div>
+              </div>
+              <div class="main-slider__title">
+                <p>Lorem</p>
+              </div>
+              <div class="main-slider__description">
+                <p>ipsum</p>
+              </div>
+            </div>
+            <div class="main-slider__img">
+              <img class="main-slider__img--active" src="<?php echo get_template_directory_uri();?>/assets/img/recomendados-1.jpg">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="main-slider__next">
+          <div class="main-slider__box">
+            <div class="main-slider__text">
+              <hr class="main-slider__line">
+              <div class="main-slider__number">
+                <span class="number--active">02</span>
+                <div class="main-slider-smaline"></div>
+              </div>
+              <div class="main-slider__title">
+                <p>Lorem</p>
+              </div>
+              <div class="main-slider__description">
+                <p>ipsum</p>
+              </div>
+            </div>
+            <div class="main-slider__img">
+              <img class="main-slider__img--active" src="<?php echo get_template_directory_uri();?>/assets/img/articulo-3.jpg">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="main-slider__next">
+          <div class="main-slider__box">
+            <div class="main-slider__text">
+              <hr class="main-slider__line">
+              <div class="main-slider__number">
+                <span class="number--active">03</span>
+                <div class="main-slider-smaline"></div>
+              </div>
+              <div class="main-slider__title">
+                <p>Lorem</p>
+              </div>
+              <div class="main-slider__description">
+                <p>ipsum</p>
+              </div>
+            </div>
+            <div class="main-slider__img">
+              <img class="main-slider__img--active" src="<?php echo get_template_directory_uri();?>/assets/img/Capadocia.jpg">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <section class="main-destiny" id="destiny">
+    <div class="container">
+      <div class="content__title">
+        <h2>Destinos</h2>
+      </div>
+      <div style="position:relative;">
+        <div class="slider-container">
+
+          <div class="slider-content">
+
+            <div class="slider-single">
+              <div style="display:grid; grid-template-columns: 50% 50%;" class="content">
+                <div class="content-items">
+                  <div class="main-destiny__box">
+                    <div class="main-destiny__title">
+                      <p>Edimburgo</p>
+                    </div>
+                    <div class="main-destiny__site">
+                      <div class="main-destiny__icon">
+                        <i></i>
+                      </div>
+                      <div class="main-destiny__text">
+                        Escocia
+                      </div>
+                    </div>
+                    <hr class="main-articles__line">
+                    <div class="main-destiny__descrption">
+                      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis aspernatur commodi
+                        perferendis,
+                        dignissimos.</p>
+                    </div>
+                  </div>
+
+                </div>
+                <img class="slider-single-image"
+                  src="<?php echo get_template_directory_uri();?>/assets/img/recomendados-1.jpg"
+                  alt="1" />
+                </a>
+              </div>
+            </div>
+            <div class="slider-single">
+              <div style="display:grid; grid-template-columns: 50% 50%;" class="content">
+                <div class="content-items">
+                  <div class="main-destiny__box">
+                    <div class="main-destiny__title">
+                      <p>Edimburgo 2</p>
+                    </div>
+                    <div class="main-destiny__site">
+                      <div class="main-destiny__icon">
+                        <i></i>
+                      </div>
+                      <div class="main-destiny__text">
+                        Escocia
+                      </div>
+                    </div>
+                    <hr class="main-articles__line">
+                    <div class="main-destiny__descrption">
+                      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis aspernatur commodi
+                        perferendis,
+                        dignissimos.</p>
+                    </div>
+                  </div>
+
+                </div>
+                <img class="slider-single-image"
+                  src="<?php echo get_template_directory_uri();?>/assets/img/recomendados-1.jpg"
+                  alt="1" />
+                </a>
+              </div>
+            </div>
+
+            <div class="slider-single">
+              <div style="display:grid; grid-template-columns: 50% 50%;" class="content">
+                <div class="content-items">
+                  <div class="main-destiny__box">
+                    <div class="main-destiny__title">
+                      <p>Edimburgo</p>
+                    </div>
+                    <div class="main-destiny__site">
+                      <div class="main-destiny__icon">
+                        <i></i>
+                      </div>
+                      <div class="main-destiny__text">
+                        Escocia
+                      </div>
+                    </div>
+                    <hr class="main-articles__line">
+                    <div class="main-destiny__descrption">
+                      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis aspernatur commodi
+                        perferendis,
+                        dignissimos.</p>
+                    </div>
+                  </div>
+
+                </div>
+                <img class="slider-single-image"
+                  src="<?php echo get_template_directory_uri();?>/assets/img/recomendados-1.jpg"
+                  alt="1" />
+                </a>
+              </div>
+            </div>
+            <div class="slider-single">
+              <div style="display:grid; grid-template-columns: 50% 50%;" class="content">
+                <div class="content-items">
+                  <div class="main-destiny__box">
+                    <div class="main-destiny__title">
+                      <p>Edimburgo</p>
+                    </div>
+                    <div class="main-destiny__site">
+                      <div class="main-destiny__icon">
+                        <i></i>
+                      </div>
+                      <div class="main-destiny__text">
+                        Escocia
+                      </div>
+                    </div>
+                    <hr class="main-articles__line">
+                    <div class="main-destiny__descrption">
+                      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis aspernatur commodi
+                        perferendis,
+                        dignissimos.</p>
+                    </div>
+                  </div>
+
+                </div>
+                <img class="slider-single-image"
+                  src="<?php echo get_template_directory_uri();?>/assets/img/recomendados-1.jpg"
+                  alt="1" />
+                </a>
+              </div>
+            </div>
+
+
+          </div>
+
+          <a class="slider-left" href="javascript:void(0);"><i class=""></i></a>
+
+          <a class="slider-right" href="javascript:void(0);"><i class=""></i></a>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="main-post">
+    <div class="container">
+      <?php if (!dynamic_sidebar('Viajero del mes')); ?>   
+          <?php if (!dynamic_sidebar('Viajero del mes imagen')); ?>
+          
+      </div>
+    </div>
+  </section>
+  <section class="main-articles">
+    <div class="container">
+      <div class="content__title">
+        <h2>últimos articulos</h2>
+      </div>
+      <div class="main-articles__content articles-xs__none">
+        <?php //Post mas recientes
+          $args = array(
+            'post_type' => 'recursos_viajeros',
+            'post_status' => 'publish',
+            'posts_per_page' => 4,
+
+            
+          );
+          $loop = new WP_Query( $args );
+        ?>
+        <?php while( $loop->have_posts() ) : $loop->the_post();?>
+          <div class="main-articles__item">
+            <a href="#">
+              <div class="main-articles__img">
+                <img class="img-round" src="<?php the_post_thumbnail_url(); ?>"/>
+              </div>
+              <div class="main-articles__title">
+                <p><?php the_title();?></p>
+                <span></span>
+              </div>
+              <div class="main-article__btn">
+                <a class="btn_custom btn--medium btn--filled" href="<?php the_permalink(); ?>">Ver más </a>
+              </div>
+            </a>
+          </div>
+        <?php endwhile;
+            wp_reset_query();
+        ?>
+      <hr class="main-articles__line">
+    </div>
+  </section>
+  <script>
+   
+    $(document).ready(function () {
+      // Add smooth scrolling to all links
+      $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+
+          // Store hash
+          var hash = this.hash;
+
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function () {
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+      });
+    });
+  </script>
+<?php get_footer(); ?>
