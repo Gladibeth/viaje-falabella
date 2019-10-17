dc0ac22bc7f5b7c997303e52a9b4bf1ea897a0c2
renny10037 <renny10037@gmail.com>
2019-10-17 14:48:05 -0500

    categoria temporada

--- development/header.php
+++ development/header.php
@@ -72,18 +72,13 @@
             <a aria-expanded="false" aria-haspopup="true" class="nav-link nav-link-active dropdown-toggle " data-toggle="dropdown" href="#" id="dropdown01">Productos</a>
             <div aria-labelledby="dropdown02" class="dropdown-menu">
               <div class="sub-drop-destiny">
-                <a class="dropdown-item" href="<?php echo bloginfo('url').'/tipo_destinos/europa';?>">
+                <a class="dropdown-item" href="<?php echo bloginfo('url').'/tipo_productos/temporada/';?>">
                     <img id="site1" src="<?php bloginfo('template_url') ?>/assets/img/icons/Icono-europa-01.svg">
                     <img id="site_1" class="display_none" src="<?php bloginfo('template_url') ?>/assets/img/icons/site1.png">
                     Temporada
                   
                   </a>
-                <a class="dropdown-item" href="<?php echo bloginfo('url').'/tipo_destinos/caribe';?>">
-                    <img id="site2" src="<?php bloginfo('template_url') ?>/assets/img/icons/Icono-caribe-01.svg">
-                    <img id="site_2" class="display_none" src="<?php bloginfo('template_url') ?>/assets/img/icons/site2.png">
-                    Producto vendido
-                  </a>
-
+                
               </div>
             </div>
           </li>
