e424dd05037ad8ed6832123b06c526883f9f6610
Gladibeth <gladibethdelos@gmail>
2019-10-22 09:21:04 -0400

    Search responsive

--- development/header.php
+++ development/header.php
@@ -30,7 +30,7 @@
       <div class="main-brand">
         <a class="navbar-brand" href="<?php echo bloginfo('url');?>">
             <img id="iso" src="<?php echo get_template_directory_uri();?>/assets/img/logo.png">
-            <!-- <img id="iso_green" class="display_none" src="<?php //echo get_template_directory_uri();?>/assets/img/logo-b.png"> -->
+            <img id="iso_green" class="display_none" src="<?php echo get_template_directory_uri();?>/assets/img/logo-b.png">
           </a>
         <button class="navbar-toggler p-2 border-0 hamburger hamburger--elastic ml-autos" data-toggle="offcanvas" type="button">
           <span class="hamburger-box">
@@ -41,24 +41,16 @@
       <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-autos">
           <div class="main-nav__search main-nav__search--lg-none  ">
-            <div class="search">
-                  <input class="" type="text" placeholder=" ">
-                  <div>
-                      <svg class="svg-white" >
-                          <use xlink:href="#path">
-                      </svg>
-                  </div>
-              </div>
-                
-              <svg class="svg-white" xmlns="http://www.w3.org/2000/svg" style="display: none;">
-                  <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 160 28" id="path">
-                      <path d="M32.9418651,-20.6880772 C37.9418651,-20.6880772 40.9418651,-16.6880772 40.9418651,-12.6880772 C40.9418651,-8.68807717 37.9418651,-4.68807717 32.9418651,-4.68807717 C27.9418651,-4.68807717 24.9418651,-8.68807717 24.9418651,-12.6880772 C24.9418651,-16.6880772 27.9418651,-20.6880772 32.9418651,-20.6880772 L32.9418651,-29.870624 C32.9418651,-30.3676803 33.3448089,-30.770624 33.8418651,-30.770624 C34.08056,-30.770624 34.3094785,-30.6758029 34.4782612,-30.5070201 L141.371843,76.386562" transform="translate(83.156854, 22.171573) rotate(-225.000000) translate(-83.156854, -22.171573)"></path>
-                  </symbol>
-              </svg>
-                
-              <a class="main-nav__mail" data-toggle="modal" href="#exampleModal">
-                  <i class="fa fa-envelope-o"></i>
-                </a>
+            <div class="content-nav icon-nav">
+                <form>
+                    <div class="search-wrapper">
+                        <input class="search-input" type="text" placeholder="Search" /><i class="fa fa-search"></i>
+                    </div>
+                </form>
+            </div>
+            <a class="main-nav__mail" data-toggle="modal" href="#exampleModal">
+              <i class="fa fa-envelope-o"></i>
+            </a>
           </div> 
           <li class="nav-item dropdown">
             <a aria-expanded="false" aria-haspopup="true" class="nav-link nav-link-active dropdown-toggle dropdown-toggle1 " data-toggle="dropdown" href="#" id="dropdown01">Destinos</a>
@@ -126,21 +118,7 @@
                     </div>
                 </form>
             </div>
-        <!-- <div class="search">
-              <input class="" type="text" placeholder=" ">
-              <div>
-                  <svg class="svg-white" >
-                      <use xlink:href="#path">
-                  </svg>
-              </div>
-          </div>
-            
-          <svg class="svg-white" xmlns="http://www.w3.org/2000/svg" style="display: none;">
-              <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 160 28" id="path">
-                  <path d="M32.9418651,-20.6880772 C37.9418651,-20.6880772 40.9418651,-16.6880772 40.9418651,-12.6880772 C40.9418651,-8.68807717 37.9418651,-4.68807717 32.9418651,-4.68807717 C27.9418651,-4.68807717 24.9418651,-8.68807717 24.9418651,-12.6880772 C24.9418651,-16.6880772 27.9418651,-20.6880772 32.9418651,-20.6880772 L32.9418651,-29.870624 C32.9418651,-30.3676803 33.3448089,-30.770624 33.8418651,-30.770624 C34.08056,-30.770624 34.3094785,-30.6758029 34.4782612,-30.5070201 L141.371843,76.386562" transform="translate(83.156854, 22.171573) rotate(-225.000000) translate(-83.156854, -22.171573)"></path>
-              </symbol>
-          </svg> -->
-            
+      
           <a class="main-nav__mail" data-toggle="modal" href="#exampleModal">
               <i class="fa fa-envelope-o"></i>
             </a>
