f56a3758fc32b853a7492cdfeb634f2cf3fd67b7
Gladibeth <gladibethdelos@gmail>
2019-11-11 12:39:19 -0400

    Add text footer

--- theme/footer.php
+++ theme/footer.php
@@ -2,7 +2,113 @@
     <div class="container">
       <hr class="main-articles__line">
       <div class="main-footer">
-          <?php if (!dynamic_sidebar('Footer')); ?>
+        <div class="main-footer__content">
+          <div class="main-footer__item">
+            <div class="main-footer__title">
+              <p>Acerca de Viajes Falabella</p>
+            </div>
+            <ul class="main-footer__ul">
+              <li>
+                <a href="">
+                  Sobre Nosotros
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Reporte de Sostenibilidad
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Trabaja con Nosotros
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="main-footer__item">
+            <div class="main-footer__title">
+              <p>Acerca de Viajes Falabella</p>
+            </div>
+            <ul class="main-footer__ul">
+              <li>
+                <a href="">
+                  Atención al Cliente
+                </a>
+                </li>
+              <li>
+                <a href="">Contáctanos</a>
+                </li>
+              <li>
+                <a href="">Sucursales</a>
+                </li>
+              <li>
+                <a href="">Web Check-In</a>
+                </li>
+              <li>
+                <a href="">Reversión de pagos</a>
+                </li>
+            </ul>
+          </div>
+
+          <div class="main-footer__item">
+            <div class="main-footer__title">
+              <p>Políticas y Condiciones</p>
+            </div>
+            <ul class="main-footer__ul">
+              <li>
+                <a href="">Política de Tratamiento de Datos</a>
+                </li>
+              <li>
+                <a href="">Ley de Retracto y Desistimiento</a>
+                </li>
+              <li>
+                <a href="">Términos y condiciones</a>
+                </li>
+              <li>
+                <a href="">Términos y condiciones de Servicios</a>
+                </li>
+              <li>
+                <a href="">Políticas de Sostenibilidad</a>
+                </li>
+              <li>
+                <a href="">Derechos y Deberes de los Pasajeros</a>
+                </li>
+            </ul>
+          </div>
+          <div class="main-footer__item">
+            <div class="main-footer__title">
+              <p>Otros Viajes</p>
+            </div>
+            <ul class="main-footer__ul">
+              <li>
+                <a href="">Atención a Empresas</a>
+                </li>
+              <li>
+                <a href="">Grupos Especiales</a>
+                </li>
+
+            </ul>
+          </div>
+    
+        </div>
+
+        <div class="main-footer__redes">
+          <div class="main-footer__redesitems">
+            <a href="#" class="main-footer__redesitem">
+              <img src="<?php echo get_template_directory_uri();?>/assets/img/f_logo_RGB-Blue_1024.svg" alt="">
+            </a>
+            <a href="#" class="main-footer__redesitem">
+              <img src="<?php echo get_template_directory_uri();?>/assets/img/Twitter_Social_Icon_Circle_Color.svg" alt="">
+            </a>
+            <a href="#" class="main-footer__redesitem">
+              <img src="<?php echo get_template_directory_uri();?>/assets/img/linked in logo circulo.svg" alt="">
+            </a>
+          </div>
+        </div>
+        <div class="main-footer__redes">
+          <p>Viajes Falabella, Colombia - 2019 - Todos los derechos reservados.</p>
+        </div>
+        <?php if (!dynamic_sidebar('Footer')); ?>
       </div>
     </div>
   </footer>
